d5f0052533e17e74706c4b7422790870a313acf3
mingleg7756 <minglegaoschool@gmail.com>
2025-04-14 11:44:42 -0400

    Initial Commit

---
+++ cardentification/cardentificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardentificationApp.swift
+//  cardentification
+//
+//  Created by Tim Cook on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct cardentificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cardentificationTests/cardentificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  cardentificationTests.swift
+//  cardentificationTests
+//
+//  Created by Tim Cook on 4/14/25.
+//
+
+import Testing
+@testable import cardentification
+
+struct cardentificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
